288e5a522dc7838ab4fae1e60363de70ca36d5c7
1izard <dee1bdnatiaw@gmail.com>
2020-02-09 04:30:42 +0900

    Add help

--- renames/__main__.py
+++ renames/__main__.py
@@ -1,5 +1,10 @@
+import argparse
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser(description='Rename files at once',
+                                     usage='Exec "$ renames" and edit file names like "src >> dst"')
+    parser.parse_args()
 
 
 if __name__ == '__main__':
